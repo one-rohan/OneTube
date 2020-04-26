0573d210ab55879348bed3dc854dcc71840d1668
one-rohan <rohanjain6@deloitte.com>
2020-04-26 22:18:47 +0530

    Added Sync Object

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,37 +1,45 @@
 var player;
 const socket = io.connect("//localhost:3000/");
 
+let videoState = {
+  videoId: "5dmQ3QWpy1Q",
+  state: -1
+};
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    playerVars: { 'autoplay': 1, 'showinfo': 0, 'playsinline': 1},
+    playerVars: { 'autoplay': 0, 'showinfo': 0, 'playsinline': 1},
     events: {
       'onStateChange': onPlayerStateChange
     },
-    videoId: ""
+    videoId: "5dmQ3QWpy1Q"
   });
 }
 
-// function onPlayerReady(e) {
-   
-//   e.target.playVideo();
-// }
-
 function onPlayerStateChange(e) {
-    socket.emit("stateChanged", {data: e.data});
+    // socket.emit("stateChanged", {data: e.data});
+    videoState.state = e.data;
 }
-
-socket.on("updateState", (state) => {
-  console.log(state);
-  if(state.data === 2)
-      player.pauseVideo();
-});
 
 document.querySelector(".url-load").addEventListener('click', (e) => {
   e.preventDefault();
   const url = document.querySelector('.url-main').value;
   const vid = url.substr(url.indexOf("=")+1);
-  console.log(vid);
+  videoState.videoId = vid;
   player.loadVideoById(vid,0); 
+});
+
+document.querySelector(".sync").addEventListener('click', () => {
+  socket.emit("stateSync", videoState);
+});
+
+socket.on("updateState", (state) => {
+  console.log(state);
+  player.loadVideoById(state.videoId, 0);
+  if(state.state === 1)
+    player.playVideo();
+  else if(state.state === 2)
+    player.pauseVideo();
 });
