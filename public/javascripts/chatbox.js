d22ed847ced8816063286fa997f91c9d51e3f4d3
one-rohan <rohanjain6@deloitte.com>
2020-04-29 00:26:17 +0530

    Added Chatbox Styles

--- public/javascripts/chatbox.js
+++ public/javascripts/chatbox.js
@@ -1,4 +1,36 @@
+let joinBtn = document.querySelector(".user-join");
+let chatList = document.querySelector(".chat-list");
+let isJoined = false;
+let username = "";
+
+joinBtn.addEventListener("click", (e) => {
+    const newNode = document.createElement("li");
+    const usernameVal = document.querySelector(".username");
+
+    if (!isJoined) {
+        username = document.querySelector(".username").value;
+        newNode.classList.add("user-joined");
+        newNode.innerHTML = 'You have joined the chat.';
+        chatList.appendChild(newNode);
+        usernameVal.value = "";
+        usernameVal.placeholder = "Type Your Message...";
+        joinBtn.innerHTML = "SEND";
+        isJoined = true;
+        socket.emit("user joined chat", username);
+    } else {
+        newNode.classList.add("user-msg");
+        newNode.innerHTML = '<span class="name">You: </span>'+ usernameVal.value;
+        chatList.appendChild(newNode);
+        usernameVal.value = "";
+    }
+});
+
 
 socket.on('user joined', (user) => {
-    document.querySelector(".user-count .users").innerHTML = user;
+    let userCount =  document.querySelector(".user-count .users");
+    userCount.innerHTML = user;
+});
+
+socket.on("show new user", () => {
+    
 });
