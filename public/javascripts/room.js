a7bf065f9559b1962c36c670a22c3ee1ebf39568
one-rohan <rohanjain6@deloitte.com>
2020-05-05 18:30:55 +0530

    Added Favicon

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,14 +1,15 @@
 const socket = io.connect();
 
 const roomModal =  $('#roomModal');
-const roomJoin = $($('.room-join'), '#roomModal');
+const roomJoin = $('#roomModal .room-join');
+let roomName;
 
 window.addEventListener('load', () => {
     roomModal.modal('show');
 });
 
 roomJoin.on('click', function() {
-    let roomName = $('input.roomname').val().toString().trim();
+    roomName = $('input.roomname').val().toString().trim();
     if(roomName) {
         socket.emit("new user", roomName);
         roomModal.modal('hide');
